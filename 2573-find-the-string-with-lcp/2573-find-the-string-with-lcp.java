4d810b82df46f8f2a148023bf9390759f5916d9d
Pranathi <152048179+saipranathi19@users.noreply.github.com>
2026-03-28 21:03:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
@@ -3,19 +3,19 @@ class Solution {
         int n = lcp.length;
         char[] word = new char[n];
 
-        // Step 1: Check diagonal (important)
+       
         for (int i = 0; i < n; i++) {
             if (lcp[i][i] != n - i) return "";
         }
 
-        // Step 2: Build string (group same positions)
+       
         char ch = 'a';
 
         for (int i = 0; i < n; i++) {
-            if (word[i] == 0) { // not assigned yet
-                if (ch > 'z') return ""; // only 26 letters
+            if (word[i] == 0) { 
+                if (ch > 'z') return "";
 
-                // assign current group
+                
                 for (int j = i; j < n; j++) {
                     if (lcp[i][j] > 0) {
                         word[j] = ch;
@@ -25,7 +25,7 @@ class Solution {
             }
         }
 
-        // Step 3: Verify LCP matrix
+        
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 int val = 0;
