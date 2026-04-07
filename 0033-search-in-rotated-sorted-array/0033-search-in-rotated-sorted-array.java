15968690b7114f5ae5584058a5cfd1e5a0a9da27
Pranathi <152048179+saipranathi19@users.noreply.github.com>
2026-04-07 21:16:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -9,20 +9,20 @@ class Solution {
                 return mid;
             }
 
-            // Check if left half is sorted
+           
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1; // search left
+                    right = mid - 1; 
                 } else {
-                    left = mid + 1;  // search right
+                    left = mid + 1;  
                 }
             } 
-            // Right half is sorted
+           
             else {
                 if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;  // search right
+                    left = mid + 1;  
                 } else {
-                    right = mid - 1; // search left
+                    right = mid - 1; 
                 }
             }
         }
