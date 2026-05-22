8790d779cd544908c13e04d5573eeb0998b0aa7d
Pranathi <152048179+saipranathi19@users.noreply.github.com>
2026-05-22 21:59:02 +0530

    Time: 22 ms (87.87%), Space: 52 MB (87.13%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,26 @@
+class KthLargest {
+
+    PriorityQueue<Integer> minHeap;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+
+        
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+       
+        return minHeap.peek();
+    }
+}
