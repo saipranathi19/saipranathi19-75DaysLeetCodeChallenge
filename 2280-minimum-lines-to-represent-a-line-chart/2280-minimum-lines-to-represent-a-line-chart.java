05361ea698dcb8448b128af12a83cdd335b07dd5
Pranathi <152048179+saipranathi19@users.noreply.github.com>
2026-06-17 21:42:51 +0530

    Time: 312 ms (5%), Space: 125.9 MB (30%) - LeetHub

---
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int minimumLines(int[][] stockPrices) {
+        int n = stockPrices.length;
+        
+        if (n <= 1) return 0;
+
+        Arrays.sort(stockPrices, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int lines = 1;
+
+        for (int i = 2; i < n; i++) {
+            long x1 = stockPrices[i - 2][0];
+            long y1 = stockPrices[i - 2][1];
+
+            long x2 = stockPrices[i - 1][0];
+            long y2 = stockPrices[i - 1][1];
+
+            long x3 = stockPrices[i][0];
+            long y3 = stockPrices[i][1];
+
+            long left = (y2 - y1) * (x3 - x2);
+            long right = (y3 - y2) * (x2 - x1);
+
+            if (left != right) {
+                lines++;
+            }
+        }
+
+        return lines;
+    }
+}
